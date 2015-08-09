d3beeb0dae12e3f8523dc4c9ba26881325b453f6
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-08-09 12:34:54 -0430

    Aggresive coding for the sake of a sane stateful architecture

--- engine/state.py
+++ engine/state.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-class State:
+import pyglet
+
+class State():
     """State implementation"""
-    def __init__(self, id, *, machine):
-        self.id = id
+    def __init__(self, *, machine):
         self.machine = machine
 
     def pop(self):
@@ -21,22 +22,60 @@ class State:
     def on_exit(self):
         pass
 
-class StateMachine:
+class StateMachine(pyglet.event.EventDispatcher):
     """Finite state machine"""
     def __init__(self):
+        super().__init__()
+
+        #
+        self.register_event_type('on_begin')
+        self.register_event_type('on_run')
+        self.register_event_type('on_exit')
+
+        #
         self.stack = []
         self.states = {}
 
-    def register(self, state):
-        self.states[state.id] = state
+    def register_state(self, id, cls):
+        self.states[id] = cls
 
-    def _get_current_state(self):
-        return self.stack[0]
+    def get_current_state(self):
+        if len(self.stack):
+            return self.stack[0]
+        return None
 
-    def pop(self):
-        self._get_current_state().on_exit()
+    def run_state(self):
+        self.dispatch_event('on_run')
+
+    def purge_stack(self):
+        while len(self.stack):
+            self.pop_state()
+
+    def pop_state(self):
+        self.dispatch_event('on_exit')
         self.stack.pop(0)
+        self._attach_events(self.get_current_state())
 
-    def push(self, id):
-        self._get_current_state().on_exit()
-        self.stack.insert(0, self.states[id])
+    def _attach_events(self, state):
+        if self._event_stack:
+            self.pop_handlers()
+        if state is not None:
+            self.set_handler('on_begin', state.on_begin)
+            self.set_handler('on_run', state.on_run)
+            self.set_handler('on_exit', state.on_exit)
+
+    def push_state(self, id):
+        #
+        state = self.get_current_state()
+        if state is not None:
+            self.dispatch_event('on_exit')
+
+        #
+        state = self.states[id](machine=self)
+
+        #
+        self.stack.insert(0, state)
+        self._attach_events(state)
+
+        #
+        self.dispatch_event('on_begin')
--- game/game.py
+++ game/game.py
@@ -1,26 +1,86 @@
 # -*- coding: utf-8 -*-
 
 import pyglet
+import sys, traceback
+from engine.state import State, StateMachine
 
-class Game:
+class GameSplash(State):
+    """Game start state"""
+
+    def __init__(self, *, machine):
+        super().__init__(machine=machine)
+        self.print_run = False
+
+    def on_begin(self):
+        print("on_begin called")
+
+    def on_run(self):
+        if not self.print_run:
+            print("on_run called")
+            self.print_run = True
+
+    def on_exit(self):
+        print("on_exit called")
+
+class Game(StateMachine):
     """Game class"""
+
     def __init__(self, argv):
+        super().__init__()
         self.window = pyglet.window.Window()
         self.window.on_draw = self.on_draw
+        self.window.on_close = self.on_window_close
+
+        #
+        self.register_state('game_splash', GameSplash)
+
+        #
+        self.shutdown = False
+
+
+    def on_window_close(self):
+        self.shutdown = True
 
     def on_draw(self):
         self.window.clear()
 
+    def _handle_except(self, e):
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print("Unhandled {e} at {file}:{line}: '{msg}'"
+              .format(e=exc_type.__name__, file=fname,
+              line=exc_tb.tb_lineno,  msg=e))
+        print(traceback.format_exc())
+
     def go(self):
         """Main entry point"""
         try:
-            pyglet.app.run()
-        except:
-            pass
+            # Push first State
+            self.push_state('game_splash')
+
+            # Run the thing!
+            while not self.shutdown:
+                #
+                pyglet.clock.tick()
+
+                #
+                self.run_state()
+
+                #
+                for window in pyglet.app.windows:
+                    window.switch_to()
+                    window.dispatch_events()
+                    window.dispatch_event('on_draw')
+                    window.flip()
+
+        except Exception as e:
+            self._handle_except(e)
+
         finally:
             self.__cleanup()
+
         return 0
 
     def __cleanup(self):
         """House keeping after all's been done"""
-        pass
+        self.purge_stack()
