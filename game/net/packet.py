e20736ecc5c91d6e12787ac4c488a1ec19ac420a
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-08-13 13:42:24 -0430

    Yet still buggy packet classes

--- game/net/packet.py
+++ game/net/packet.py
@@ -1,34 +1,53 @@
 # -*- coding: utf-8 -*-
 
-class PacketRequest(object):
+class PacketRequest:
     CMD_CONNECT = 'connect'
-    def __init__(self, data):
-        self._data = data
+    def __init__(self, data={}):
+        self._req = data
 
     @property
     def command(self):
-        if 'cmd' in self._data:
-            return self._data['cmd']
+        if 'cmd' in self._req:
+            return self._req['cmd']
         return None
 
     @command.setter
     def command(self, value):
-        self._data['cmd'] = value
+        self._req['cmd'] = value
 
-class PacketResponse(object):
+    @property
+    def data(self):
+        return self._req
+
+
+class PacketResponse:
     RES_OK = 'OK'
     RES_NOT_OK = 'NOT_OK'
 
-    def __init__(self, *, response=RES_NOT_OK):
-        self._res = {"res":response}
+    REASON_CONN_REFUSED = "conn_refused"
+
+    def __init__(self, data={}):
+        self._res = data
 
     @property
     def response(self):
-        return self._res['res']
+        if 'res' in self._res:
+            return self._res['res']
+        return None
 
     @response.setter
     def response(self, value):
         self._res['res'] = value
+
+    @property
+    def reason(self):
+        if 'reason' in self._res:
+            return self._res['reason']
+        return None
+
+    @reason.setter
+    def reason(self, value):
+        self._res['reason'] = value
 
     @property
     def player_id(self):
