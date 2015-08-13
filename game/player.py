ef9bc28d29cde40c4a8509cc5328231203c7979e
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-08-13 13:44:05 -0430

    PlayerClient's poor protocol implementation for authentication against server

--- game/player.py
+++ game/player.py
@@ -2,9 +2,22 @@
 
 from engine.net import Client
 
+from game.net.packet import PacketRequest, PacketResponse
+
 class PlayerClient(Client):
+    def connect(self):
+        req = PacketRequest()
+        req.command = PacketRequest.CMD_CONNECT
+        self.send(req.data)
+
     def on_data_received(self, data, host, port):
-        print(data)
+        res = PacketResponse(data)
+
+        if res.response == PacketResponse.RES_NOT_OK:
+            if res.reason == PacketResponse.REASON_CONN_REFUSED:
+                raise ConnectionRefusedError(
+                    "Connection to {}:{} refused!".format(
+                    self._server_host, self._server_port))
 
     def on_key_press(self, symbol, modifiers):
         # maybe send packets to the server as the player hits buttons
