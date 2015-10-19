502716e542f92ef1f5c90e208adf257854addecb
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-10-19 10:42:26 -0430

    credits state makeup

--- game/states/credits.py
+++ game/states/credits.py
@@ -37,7 +37,7 @@ class CreditsState(BaseState):
                 font_size=12, y=((self.window.height//2) - 20))
         self._title_label.set_style('color', (240, 240, 240, 255))
 
-        #
+        # set the background color
         self.set_background_color(31, 31, 31)
 
 
@@ -46,10 +46,12 @@ class CreditsState(BaseState):
     #
 
     def on_begin(self):
+        # schedule a transition to the next state
         pyglet.clock.schedule_once(self._trans_splash, 2)
 
 
     def _trans_splash(self, dt):
+        # trigger a transition to the splash state
         self.transition_to('game_splash')
 
 
@@ -57,5 +59,6 @@ class CreditsState(BaseState):
         # Draw labels
         self._title_label.draw()
         self._presents_label.draw()
-        super().on_update()
+        super().on_update() # draw things on parent
+
 
