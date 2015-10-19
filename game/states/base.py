aaad175f3791084967ff835d97cb8c0fc48a1beb
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-10-19 11:15:43 -0430

    entire makeup for base state

--- game/states/base.py
+++ game/states/base.py
@@ -37,15 +37,15 @@ class BaseState(State):
         self._server = spot_get('game_server')
         self._client = spot_get('game_client')
 
-        #
+        # initial values for fade animation flags and alpha
         self._fade_out = False
         self._fade_in  = False
         self._fade_alpha = 0
 
-        #
+        # get the sorcerer to use resources
         self.sorcerer = spot_get('game_object').sorcerer
 
-        #
+        # create the base font used throughout the entire game
         self.sorcerer.create_font('8-bit Operator+',
                 file_name='8bitOperatorPlus-Regular.ttf')
 
@@ -55,7 +55,22 @@ class BaseState(State):
             x=None, y=None,
             bold=False,
             anchor_x='center', anchor_y='center'):
+        """ Create a pyglet label easily
 
+        Args:
+            text(str): Initial text for this resource
+
+        Kwargs:
+            font_size(int, optional): font size
+            x(int, optional): horizontal position
+            y(int, optional): vertical position
+            bold(bool, optional): whether or not this label is going to be rendered as bold text
+            anchor_x(int, optional): horizontal anchor for this label
+            anchor_y(int), optional: vertical anchor for this label
+        """
+
+        # by default, a label created under this method
+        # is going to be centered if no position has been specified
         if x is None:
             pos_x = self.window.width // 2
         else:
@@ -66,6 +81,7 @@ class BaseState(State):
         else:
             pos_y = y
 
+        # create the actual thing
         return pyglet.text.Label(
             text, font_name='8-bit Operator+', font_size=font_size,
             x=pos_x, y=pos_y, bold=bold,
@@ -74,20 +90,46 @@ class BaseState(State):
 
 
     def set_background_color(self, red, green, blue, alpha=255):
-        """ Set background color """
+        """ Set background color
+
+        Args:
+            red(int): amount of red in this color
+            green(int): amount of green in this color
+            blue(int): amount of blue in this color
+            alpha(int): amount of alpha in this color
+        """
         pyglet.gl.glClearColor(red/255, green/255, blue/255, alpha/255)
 
 
     def _setup_fade(self, fade_func, total_time, alpha):
+        """Set up fade animation and schedule it"""
+
+        # total fade animation time in seconds
         self._fade_total_time = total_time/1000
+
+        # updates to animation are going to be made at 60 fps
         fade_interval = 1/60.0
+
+        # the amount of alpha applied to the fade polygon is relative to both
+        # the total time of the animation and its interval time
         self._fade_step = int( 255 / (self._fade_total_time/fade_interval) )
+
+        # actually schedule the animation under the set interval
         pyglet.clock.schedule_interval(fade_func, fade_interval)
+
+        # set initial alpha
         self._fade_alpha = alpha
 
 
     def transition_to(self, state_name, *, total_time=100):
-        """ Trigger animated transition to state_name """
+        """ Trigger animated transition to state_name
+
+        Args:
+            state_name(str): key name of the state to push after fade in/out
+        Kwargs:
+            total_time(int, optional): animation total time in milliseconds
+        """
+
         self._fade_out = True
         self._trans_state_name = state_name
         self._setup_fade(self._fade_alpha_up, total_time, 0)
@@ -102,7 +144,8 @@ class BaseState(State):
 
 
     def on_exit(self):
-        glDisable(GL_BLEND)
+        """Clean up everyhing before exiting"""
+
         pyglet.clock.unschedule(self._fade_alpha_up)
         pyglet.clock.unschedule(self._fade_alpha_down)
         self._fade_in = False
@@ -124,9 +167,10 @@ class BaseState(State):
                             width, height,
                             0, height)
                     ),
-                    # ('c3B',(10, 25, 0) * 6 )
                     ('c4B', (0, 0, 0, self._fade_alpha) * 6)
                 )
+            glDisable(GL_BLEND)
+
             # Stop "fade" animation
             if self._fade_alpha >= 255:
                 self.push(self._trans_state_name)
@@ -134,11 +178,13 @@ class BaseState(State):
 
     @property
     def server(self):
+        """Get the running server"""
         return self._server
 
 
     @property
     def client(self):
+        """Get the running client"""
         return self._client
 
 
