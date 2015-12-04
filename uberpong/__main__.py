9096508b840e76663dd0c02195b684867a5faec1
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-12-04 19:04:43 -0430

    a main function is required in a gui_script

--- uberpong/__main__.py
+++ uberpong/__main__.py
@@ -12,5 +12,10 @@ See LICENSE for more details.
 import sys
 from uberpong.game.game import Game
 
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    return Game(sys.argv[1:]).go()
+
 if __name__=='__main__':
-    sys.exit(Game(sys.argv[1:]).go())
+    sys.exit(main())
